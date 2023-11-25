149dca2692faf63f933632bbb843f41fd0d3090a
Essie Schlotterbeck <esther.schlotterbeck@gmail.com>
2023-11-25 17:45:27 -0600

    added toggle function to toggle image and description

--- src/components/GalleryList/GalleryItem.jsx
+++ src/components/GalleryList/GalleryItem.jsx
@@ -17,16 +17,46 @@ function GalleryItem({ item, getList }) {
         });
     }
 
+    const [onDescription, setOnDescription] = useState(false)
+
+    const toggleDescription = () => {
+        setOnDescription(!onDescription);
+    }
+
+    const displayDescription = () => {
+
+        if (onDescription) {
+            return (
+                <>
+                <>{item.description}</>
+                </>
+            )
+        } else {
+            return (
+                <div>
+                <img 
+                    src={item.url} 
+                    data-testid="toggle" 
+                    onClick={toggleDescription}></img>
+  
+            </div>
+            )
+        }
+    }
+
     return (
         <div
             key={item.id}
             className="gallery-item"
             data-testid="galleryItem"
+            onClick={toggleDescription}
         >
-            <img src={item.url} data-testid="toggle"></img>
+            {displayDescription()}
+            {/* <img 
+                src={item.url} 
+                data-testid="toggle">
+                </img> */}
             <br></br>
-            {/* <div>Rocks Picked: {rockCounter} {rockCounter >= 50 ? 'done' : ''}</div> */}
-            {/* <button onClick={rockCounter > 0 ? decrementRockCounter : 0}>Decrease</button> */}
             <button onClick={likeCounter} id={item.id} data-testid="like">love it!</button>
             <p>{item.likes > 0 ? item.likes + ' people love this!' : ''}</p>
             <p>{item.title}</p>
